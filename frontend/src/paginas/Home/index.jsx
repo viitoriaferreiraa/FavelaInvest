25f9001a10b26288a68ff7f91ffed4d75f33da80
rafael-santos-347 <rafah00@outlook.com>
2021-02-09 23:08:16 -0300

    Finalizando Header

---
+++ frontend/src/paginas/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header";
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <Header />
+            <main><h1>olá mundo</h1></main>
+            
+
+        </React.Fragment>
+    );
+}
+
+export default Home;
